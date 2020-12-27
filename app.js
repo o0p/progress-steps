c194494b7919c7f1832bf3a5f3dbd31a34137587
Russell <russelldev@protonmail.com>
2020-12-27 08:04:17 +0000

    Add progress bar movement with btn

--- app.js
+++ app.js
@@ -30,4 +30,7 @@ function update(){
             circle.classList.remove('active');
         }
     });
+
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%';
 }
