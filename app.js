c881910de39bba9d82e72fc4d7add4663f33e1dd
Russell <russelldev@protonmail.com>
2020-12-27 07:45:42 +0000

    Add active circles response

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const circle = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
 
 next.addEventListener('click', ()=>{
     currentActive++;
-    if(currentActive > circle.length) {
-        currentActive = circle.length;
+    if(currentActive > circles.length) {
+        currentActive = circles.length;
     }
     update();
 });
@@ -21,3 +21,13 @@ prev.addEventListener('click', ()=>{
 
     update();
 });
+
+function update(){
+    circles.forEach((circle, i) => {
+        if(i < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+}
